c4973eef1b1f3d49d18cb5eff0ac1683a8462040
Michael Lima <michaellmo@outlook.com>
2022-04-18 07:45:47 -0300

    Atividade nova

---
+++ uri1011/uri1011/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uri1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159, raio;
+            raio = double.Parse(Console.ReadLine());
+
+            double volume = (4.0 / 3.0) * pi * Math.Pow(raio, 3);
+
+            Console.WriteLine("VOLUME = " +volume.ToString("F3"));
+
+        }
+    }
+}
