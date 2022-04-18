032186ad1954ef1ba5144319d505bc7b03e6766f
Michael Lima <michaellmo@outlook.com>
2022-04-18 13:52:35 -0300

    atividade nova

--- uri1012/uri1012/Program.cs
+++ uri1012/uri1012/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace uri1012
 {
@@ -8,9 +9,9 @@ namespace uri1012
         {
             double pi = 3.14159;
             string[] valores = (Console.ReadLine().Split(' '));
-            double a = double.Parse(valores[0]);
-            double b = double.Parse(valores[1]);
-            double c = double.Parse(valores[2]);
+            double a = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            double b = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(valores[2], CultureInfo.InvariantCulture);
 
             double areaTrianguloRetangulo = (a * c) / 2;
             double areaCirculo = pi * (Math.Pow(c, 2));
@@ -18,10 +19,11 @@ namespace uri1012
             double areaDoQuadrado = b * b;
             double areaDoRetangulo = a * b;
 
-            Console.WriteLine("TRIANGULO: ");
-
-
-
+            Console.WriteLine("TRIANGULO: " + areaTrianguloRetangulo.ToString("F3"));
+            Console.WriteLine("CIRCULO: " + areaCirculo.ToString("F3"));
+            Console.WriteLine("TRAPEZIO: " + areaDoTrapezio.ToString("F3"));
+            Console.WriteLine("QUADRADO: " + areaDoQuadrado .ToString("F3"));
+            Console.WriteLine("RETANGULO: " + areaDoRetangulo.ToString("F3"));
         }
     }
 }
