ef9cdec475c8d56063b0aeb24127f0a20d59f2c3
Michael Lima <michaellmo@outlook.com>
2022-04-19 15:36:07 -0300

    atividades

---
+++ Exemplos_de_bliblioteca_linq_cs/Exemplos_de_bliblioteca_linq_cs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Exemplos_de_bliblioteca_linq_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior = Console.ReadLine()
+                               .Split(' ')
+                               .Select(e => int.Parse(e))
+                               .ToArray()
+                               .Max();
+            Console.WriteLine(maior + " eh o maior");
+        }
+    }
+}
