30f1df69628cccfe630927fb7891d32efdc8646a
Michael Lima <michaellmo@outlook.com>
2022-04-20 04:02:52 -0300

    atividades

---
+++ uri1019/uri1019/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uri1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas, minutos, segundos, resto;
+            int n = int.Parse(Console.ReadLine());
+
+            horas = n / 3600;
+            resto = n % 3600;
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+
+
+        }
+    }
+}
