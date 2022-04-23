40905f0c4bed1edbb05ff301d4231775f487d320
Michael Lima <michaellima.contato@gmail.com>
2022-04-23 00:03:23 -0300

    Atividades novas

---
+++ Atividade_Estrutura_Condicional/Atividade_Estrutura_Condicional/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Atividades_Estrutura_Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas;
+
+            Console.Write("Quantas horas? ");
+            horas = int.Parse(Console.ReadLine());
+
+            if(horas < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }else if(horas < 18)
+            {
+                Console.WriteLine(" Boa tarde!");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite!");
+            }
+        }
+    }
+} 
---
+++ Exercicios_Condicionais_cs/Exercicios_Condicionais_cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios_Condicionais_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double nota2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double notaFinal = nota1 + nota2;
+
+            Console.WriteLine("NOTA FINAL = " + notaFinal.ToString("F1"),CultureInfo.InvariantCulture);
+            if(notaFinal < 60.00)
+            {
+                Console.WriteLine("REPROVADO");
+            }
+        }
+    }
+}
