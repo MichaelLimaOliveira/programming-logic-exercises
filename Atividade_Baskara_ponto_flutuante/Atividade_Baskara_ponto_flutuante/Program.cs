d082b79e54f9b431778936557c0dafa5b9e08a04
Michael Lima <michaellima.contato@gmail.com>
2022-04-23 02:21:07 -0300

    merge dos pcs

---
+++ Atividade_Baskara_ponto_flutuante/Atividade_Baskara_ponto_flutuante/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Atividade_Baskara_ponto_flutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            double a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            double b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            double c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            double delta = (Math.Pow(b, 2) - 4 * a * c);
+
+            double r1 = ((-b) + Math.Sqrt(delta)) / (2 * a);
+            double r2 = ((-b) - Math.Sqrt(delta)) / (2 * a);
+
+            if (a == 0 || delta < 0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else
+            {
+                Console.WriteLine("R1 = " + r1.ToString("F5"), CultureInfo.InvariantCulture);
+                Console.WriteLine("R2 = " + r2.ToString("F5"), CultureInfo.InvariantCulture);
+            }
+
+
+
+
+        }
+    }
+}
