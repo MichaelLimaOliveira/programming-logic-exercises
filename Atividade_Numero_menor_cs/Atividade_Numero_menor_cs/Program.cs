a27a0e3383f746cfefca5f10f5aa028530e3269d
Michael Lima <michaellima.contato@gmail.com>
2022-04-23 23:02:07 -0300

    Atividades novas

---
+++ Atividade_Numero_menor_cs/Atividade_Numero_menor_cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Atividade_Numero_menor_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int x = int.Parse(vet[0]);
+            int y = int.Parse(vet[1]);
+            int z = int.Parse(vet[2]);
+
+            if (x < y && x < z)
+            {
+                Console.WriteLine("MENOR = " + x);
+            }
+            else if (y < z)
+            {
+                Console.WriteLine("MENOR = " + y);
+            }
+            else
+            {
+                Console.WriteLine("MENOR = " + z);
+            }
+        }
+    }
+}
