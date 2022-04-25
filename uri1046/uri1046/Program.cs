43184a10cc6ff7360f09f2d9824e42d1490d88df
Michael Lima <michaellima.contato@gmail.com>
2022-04-25 00:52:55 -0300

    Atividades Novas

---
+++ uri1046/uri1046/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace uri1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado;
+            string[] vet = Console.ReadLine().Split(' ');
+            int horaInicial = int.Parse(vet[0]);
+            int horaFinal = int.Parse(vet[1]);
+
+            if (horaInicial > horaFinal)
+            {
+                resultado = (24 - horaInicial) + horaFinal;
+                Console.WriteLine("O JOGO DUROU " + resultado + " HORA(S)");
+            }
+            else if (horaInicial < horaFinal)
+            {
+                resultado = horaFinal - horaInicial;
+                Console.WriteLine("O JOGO DUROU " + resultado + " HORA(S)");
+            }
+            else
+            {
+                resultado = 24;
+                Console.WriteLine("O JOGO DUROU " + resultado + " HORA(S)");
+            }
+        }
+    }
+}
---
+++ uri1048/uri1048/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double novoSalario, valorDeReajuste;
+            double salario = double.Parse(Console.ReadLine());
+
+            if (salario >= 0.00 && salario <= 400.00)
+            {
+                valorDeReajuste = 15 * salario / 100;
+                novoSalario = salario + valorDeReajuste;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2"));
+                Console.WriteLine("Reajuste ganho: " + valorDeReajuste.ToString("F2"));
+                Console.WriteLine("Em percentual: 15 %");
+            }
+            else if (salario >= 400.01 && salario <= 800.00)
+            {
+                valorDeReajuste = 12 * salario / 100;
+                novoSalario = salario + valorDeReajuste;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2"));
+                Console.WriteLine("Reajuste ganho: " + valorDeReajuste.ToString("F2"));
+                Console.WriteLine("Em percentual: 12 %");
+            }
+            else if (salario >= 800.01 && salario <= 1200.00)
+            {
+                valorDeReajuste = 10 * salario / 100;
+                novoSalario = salario + valorDeReajuste;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2"));
+                Console.WriteLine("Reajuste ganho: " + valorDeReajuste.ToString("F2"));
+                Console.WriteLine("Em percentual: 10 %");
+
+            }
+            else if (salario >= 1200.01 && salario <= 2000.00)
+            {
+                valorDeReajuste = 7 * salario / 100;
+                novoSalario = salario + valorDeReajuste;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2"));
+                Console.WriteLine("Reajuste ganho: " + valorDeReajuste.ToString("F2"));
+                Console.WriteLine("Em percentual: 7 %");
+
+            }
+            else if (salario >= 2000.01)
+            {
+                valorDeReajuste = 4 * salario / 100;
+                novoSalario = salario + valorDeReajuste;
+                Console.WriteLine("Novo salario: " + novoSalario.ToString("F2"));
+                Console.WriteLine("Reajuste ganho: " + valorDeReajuste.ToString("F2"));
+                Console.WriteLine("Em percentual: 4 %");
+            }
+
+
+        }
+    }
+}
