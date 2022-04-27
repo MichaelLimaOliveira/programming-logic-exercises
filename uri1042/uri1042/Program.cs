d241ee27d4f3f288a8b74ec8b50bb7ce4329f573
Michael Lima <michaellima.contato@gmail.com>
2022-04-27 13:29:45 -0300

    Melhorias nos codigos

--- uri1042/uri1042/Program.cs
+++ uri1042/uri1042/Program.cs
@@ -7,12 +7,7 @@ namespace uri1042
     {
         static void Main(string[] args)
         {
-            int numberInOrder1,
-                numberInOrder2, 
-                numberInOrder3,
-                number1,
-                number2,
-                number3;
+
             int[] numbers = Console.ReadLine().Split(' ')
                                               .Select(number => int.Parse(number))
                                               .ToArray();
@@ -20,26 +15,18 @@ namespace uri1042
             int[] numerosOrdenados = numbers.OrderBy(number => number)
                                             .ToArray();
 
-            numberInOrder1 = numerosOrdenados[0];
-            numberInOrder2 = numerosOrdenados[1];
-            numberInOrder3 = numerosOrdenados[2];
+            for (int i = 0; i < numerosOrdenados.Length; i++)
+            {
+                Console.WriteLine(numerosOrdenados[i]);
 
-            number1 = numbers[0];
-            number2 = numbers[1];
-            number3 = numbers[2];
+            }
 
-
-            
-
-            
-            Console.WriteLine(numberInOrder1);
-            Console.WriteLine(numberInOrder2);
-            Console.WriteLine(numberInOrder3);
             Console.WriteLine();
-            Console.WriteLine(number1);
-            Console.WriteLine(number2);
-            Console.WriteLine(number3);
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
 
         }
     }
