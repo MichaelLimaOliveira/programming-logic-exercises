8e332e74fe3bb4b169e3f2a15f78e11c86f6ab1d
Michael Lima <michaellima.contato@gmail.com>
2022-04-27 13:18:45 -0300

    atividades novas

---
+++ uri1042/uri1042/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace uri1042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInOrder1,
+                numberInOrder2, 
+                numberInOrder3,
+                number1,
+                number2,
+                number3;
+            int[] numbers = Console.ReadLine().Split(' ')
+                                              .Select(number => int.Parse(number))
+                                              .ToArray();
+
+            int[] numerosOrdenados = numbers.OrderBy(number => number)
+                                            .ToArray();
+
+            numberInOrder1 = numerosOrdenados[0];
+            numberInOrder2 = numerosOrdenados[1];
+            numberInOrder3 = numerosOrdenados[2];
+
+            number1 = numbers[0];
+            number2 = numbers[1];
+            number3 = numbers[2];
+
+
+            
+
+            
+            Console.WriteLine(numberInOrder1);
+            Console.WriteLine(numberInOrder2);
+            Console.WriteLine(numberInOrder3);
+            Console.WriteLine();
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            Console.WriteLine(number3);
+
+
+        }
+    }
+}
