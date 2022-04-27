cf7276b9b0b14fc7be2932bafd228e969a25edd7
Michael Lima <michaellima.contato@gmail.com>
2022-04-27 18:16:36 -0300

    atividades novas

---
+++ uri1043/uri1043/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace uri1043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo.
+            //Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem: Perimetro = XX.X
+            //Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem: Area = XX.X
+            double a,
+                   b, 
+                   c, 
+                   perimetro, 
+                   area;
+
+            double[] lados = Console.ReadLine()
+                                    .Split(' ')
+                                    .Select(lado => double.Parse(lado))
+                                    .ToArray();
+
+            a = lados[0];
+            b = lados[1];
+            c = lados[2];
+
+            bool isTriangle = a + b > c && a + c > b && b + c > a;
+
+            if (isTriangle)
+            {
+                //perimetro do triangulo
+                perimetro = a + b + c;
+                Console.WriteLine("Perimetro = " + perimetro.ToString("F1"));
+            }
+            else
+            {
+                //area do trapezio
+                area = ((a + b) * c) / 2;
+                Console.WriteLine("Area = " + area.ToString("F1"));
+            }
+        }
+    }
+}
---
+++ uri1050/uri1050/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace uri1050
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int brasilia = 61,
+                salvador = 71,
+                saoPaulo = 11,
+                rioDeJaneiro = 21,
+                juizDeFora = 32,
+                campinas = 19,
+                vitoria = 27,
+                beloHorizonte = 31;
+
+            string cidade = " ";
+
+            int ddd = int.Parse(Console.ReadLine());
+
+            if (ddd == brasilia)
+            {
+                cidade = "Brasilia";
+            }
+            else if (ddd == salvador)
+            {
+                cidade = "Salvador";
+            }
+            else if (ddd == saoPaulo)
+            {
+                cidade = "Sao Paulo";
+            }
+            else if (ddd == rioDeJaneiro)
+            {
+                cidade = "Rio de Janeiro";
+            }
+            else if (ddd == juizDeFora)
+            {
+                cidade = "Juiz de Fora";
+            }
+            else if (ddd == campinas)
+            {
+                cidade = "Campinas";
+            }
+            else if (ddd == vitoria)
+            {
+                cidade = "Vitoria";
+            }
+            else if (ddd == beloHorizonte)
+            {
+                cidade = "Belo Horizonte";
+            }
+            else
+            {
+                cidade = "DDD nao cadastrado";
+            }
+
+            Console.WriteLine(cidade);   
+        }
+    }
+}
