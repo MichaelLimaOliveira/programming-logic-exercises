fca74cfefe1cdc2c4dd5af65941240d748e70a65
Michael Lima <michaellima.contato@gmail.com>
2022-04-27 00:21:29 -0300

    Melhoria 1051

--- uri1051/uri1051/Program.cs
+++ uri1051/uri1051/Program.cs
@@ -9,7 +9,7 @@ namespace uri1051
         {
             
             decimal salary = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            decimal valorImposto,
+            decimal valorImposto = 0,
                     valorIsento = 2000,
                     aliquotaMinima = 0.08m,
                     aliquotaMedia = 0.18m,
@@ -18,33 +18,33 @@ namespace uri1051
                     rangeAliquotaMedia = 1500;
 
             bool isIsento = salary >= 0 && salary <= 2000;
-            bool isAliquotaMinima = salary > 2000 && salary <= 3000;
-            bool isAliquotaMedia = salary > 3000 && salary <= 4500;
-            bool isAliquotaMaxima = salary > 4500;
-
             if (isIsento)
             {
                 Console.WriteLine("Isento");
+                return;
             }
-            else if (isAliquotaMinima)
+
+            bool isAliquotaMinima = salary > 2000 && salary <= 3000;
+            bool isAliquotaMedia = salary > 3000 && salary <= 4500;
+            bool isAliquotaMaxima = salary > 4500;
+            
+            if (isAliquotaMinima)
             {
                 valorImposto = (salary - valorIsento) * aliquotaMinima;
-                Console.WriteLine("R$ " + valorImposto.ToString("F2"), CultureInfo.InvariantCulture);
             }
             else if (isAliquotaMedia)
             {
 
                 valorImposto = (salary - valorIsento - rangeAliquotaMinima);
                 valorImposto = (rangeAliquotaMinima * aliquotaMinima) + (valorImposto * aliquotaMedia);
-                Console.WriteLine("R$ " + valorImposto.ToString("F2"), CultureInfo.InvariantCulture);
             }
             else if (isAliquotaMaxima)
             {
                 valorImposto = (salary - valorIsento - rangeAliquotaMinima - rangeAliquotaMedia);
                 valorImposto = (rangeAliquotaMinima * aliquotaMinima) + (rangeAliquotaMedia * aliquotaMedia) + (valorImposto * aliquotaMaxima);
-                Console.WriteLine("R$ " + valorImposto.ToString("F2"), CultureInfo.InvariantCulture);
-
             }
+
+            Console.WriteLine("R$ " + valorImposto.ToString("F2"), CultureInfo.InvariantCulture);
         }
     }
 }
