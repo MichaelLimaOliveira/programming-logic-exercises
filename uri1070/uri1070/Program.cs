f95d00013412886cecfa5ead27c8c7e55c388eb0
Michael Lima <michaellima.contato@gmail.com>
2022-04-27 22:45:03 -0300

    Atividades

---
+++ uri1070/uri1070/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace uri1070
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            bool isPar = (number % 2) == 0;
+            bool isImpar = (number % 2) != 0;
+            int nextNumber;
+
+            if (isPar)
+            {
+                nextNumber = number + 1;
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.WriteLine(nextNumber);
+                    nextNumber += 2;
+
+                }
+            }
+
+            if (isImpar)
+            {
+                Console.WriteLine(number);
+                nextNumber = number + 2;
+                for(int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(nextNumber);
+                    nextNumber += 2;
+                }
+            }
+        }
+    }
+}
