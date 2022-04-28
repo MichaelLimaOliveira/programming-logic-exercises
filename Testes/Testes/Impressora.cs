b1fcb0aea8ac8c809165d14d5c8d51878815362f
Michael Lima <michaellima.contato@gmail.com>
2022-04-28 20:09:55 -0300

    Primeiro contato com metodos

---
+++ Testes/Testes/Impressora.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodos
+{
+    class Impressora
+    {
+        public static void ImpressoraMundial(string print)
+        {
+            Console.WriteLine(print);
+        }
+    }
+}
