ba6bcaf39708ed67fbb669b141283f399afe4276
Michael Lima <michaellima.contato@gmail.com>
2022-04-27 22:13:36 -0300

    Melhorias

---
+++ uri1065/uri1065/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uri1065
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPar = 0, countPar = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                isPar = numbers % 2;
+
+                if (isPar == 0)
+                {
+                    countPar++;
+                    
+                }
+
+            }
+            Console.WriteLine(countPar + " valores pares");
+        }
+    }
+}
