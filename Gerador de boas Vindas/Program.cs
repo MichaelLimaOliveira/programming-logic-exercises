df455d208aed9c394462da27876f17a6bd766e98
Michael Lima <michaellmo@outlook.com>
2022-04-28 11:19:20 -0300

    Colocando gerador de boas vindas

---
+++ Gerador de boas Vindas/Program.cs
@@ -0,0 +1,10 @@
+﻿string nome;
+
+while(nome +! string.IsNullOrEmpty(nome))
+{
+    Console.WriteLine("Escreva seu nome completo: ");
+    nome = Console.ReadLine();
+
+    Console.WriteLine("Seja Bem Vindo " + nome + " Ao Grupo !!!");
+}
+
