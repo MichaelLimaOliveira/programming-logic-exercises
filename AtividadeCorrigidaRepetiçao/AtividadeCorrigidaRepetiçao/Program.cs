ea123af3c47f9dde596186cf55b0d2d5c44cc4e1
Michael Lima <michaellima.contato@gmail.com>
2022-04-29 14:20:41 -0300

    Atividades de aprendizado e exercicios do uri

---
+++ AtividadeCorrigidaRepetiçao/AtividadeCorrigidaRepetiçao/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace AtividadeCorrigidaRepetiçao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double media, soma = 0.00;
+            int quantidade = 0;
+            double numeros = double.Parse(Console.ReadLine());
+
+            while (numeros > 0)
+            {
+                quantidade++;
+                soma += numeros;
+                numeros = int.Parse(Console.ReadLine());
+
+            }
+
+            if (quantidade == 0)
+            {
+                Console.WriteLine("Imposivel calcular");
+            }
+            else
+            {
+                media = soma / quantidade;
+                Console.WriteLine(media.ToString("F2"));
+            }
+        }
+    }
+}
---
+++ uri1114EstruturaDeRepetiçao/uri1114EstruturaDeRepetiçao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace uri1114EstruturaDeRepetiçao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int correctCode = 2002;
+            int code = int.Parse(Console.ReadLine());
+
+            while (code != correctCode)
+            {
+                code = int.Parse(Console.ReadLine());
+                Console.WriteLine("Senha Invalida");
+            }
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
