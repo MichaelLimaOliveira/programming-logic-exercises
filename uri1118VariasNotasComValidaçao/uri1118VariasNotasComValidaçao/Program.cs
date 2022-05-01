0836edcb6f8082ea2f0f62b26eb752958db26eaf
Michael Lima <michaellima.contato@gmail.com>
2022-05-01 16:53:40 -0300

    Atividades Novas

---
+++ uri1118VariasNotasComValidaçao/uri1118VariasNotasComValidaçao/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace uri1118VariasNotasComValidaçao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int novoCalculo = 1;
+
+            while (novoCalculo == 1)
+            {
+                int quantidadeDeNotas = 0;
+                double media = 0.00,
+                       somaDasNotas = 0.00;
+
+                while (quantidadeDeNotas < 2)
+                {
+                    double notas = double.Parse(Console.ReadLine());
+                    if (notas >= 0 && notas <= 10)
+                    {
+                        somaDasNotas += notas;
+                        quantidadeDeNotas++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("nota invalida");
+                    }
+
+                }
+                media = somaDasNotas / 2;
+                Console.WriteLine("media = " + media.ToString("F2"));
+
+                do
+                {
+
+
+                    Console.WriteLine("novo calculo (1-sim 2-nao)");
+                    novoCalculo = int.Parse(Console.ReadLine());
+                } while (novoCalculo != 1 && novoCalculo != 2);
+            }
+        }
+    }
+}
---
+++ uri1131Grenais/uri1131Grenais/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace uri1131Grenais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int novoGrenal = 1;
+            int inter = 0, gremio = 0, empate = 0;
+            int grenais = 0;
+            string venceuMais;
+
+            while (novoGrenal == 1)
+            {
+
+
+                int[] placar = Console.ReadLine()
+                                       .Split(' ')
+                                       .Select(e => int.Parse(e))
+                                       .ToArray();
+
+                if (placar[0] > placar[1])
+                {
+                    inter++;
+                }
+                else if (placar[0] < placar[1])
+                {
+                    gremio++;
+                }
+                else
+                {
+                    empate++;
+                }
+
+
+                do
+                {
+
+
+                    Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                    novoGrenal = int.Parse(Console.ReadLine());
+
+                } while (novoGrenal != 1 && novoGrenal != 2);
+                grenais++;
+
+            }
+            Console.WriteLine(grenais + " grenais");
+            Console.WriteLine("Inter:" + inter);
+            Console.WriteLine("Gremio:" + gremio);
+            Console.WriteLine("Empates:" + empate);
+
+            if (inter > gremio)
+            {
+                venceuMais = "Inter";
+            }
+            else if (inter < gremio)
+            {
+                venceuMais = "Gremio";
+
+            }
+            else
+            {
+                venceuMais = "Nao houve vencedor";
+            }
+            Console.WriteLine(venceuMais + " venceu mais");
+
+
+
+
+        }
+    }
+}
