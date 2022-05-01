3a2de4a6df3ed3c8473c91a65fbc11ef6eddb970
Michael Lima <michaellima.contato@gmail.com>
2022-05-01 20:19:59 -0300

    exercicios resolvidos

---
+++ uri1078Tabuada/uri1078Tabuada/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uri1078Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int soma = numero * i;
+                Console.WriteLine(i + " x " + numero + " = " +soma);
+            }
+        }
+    }
+}
