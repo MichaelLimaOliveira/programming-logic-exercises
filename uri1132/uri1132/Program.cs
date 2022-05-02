f16e20159ae7731ed84ff317ed89dd4b7e597d9e
Michael Lima <michaellima.contato@gmail.com>
2022-05-02 20:56:56 -0300

    atividades novas

---
+++ uri1132/uri1132/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace uri1132
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int maior, 
+                menor, 
+                soma = 0;
+
+            if (number1 > number2)
+            {
+                maior = number1;
+                menor = number2;
+            }
+            else
+            {
+                maior = number2;
+                menor = number1;
+            }
+
+            for(int i = menor; i<=maior; i++)
+            {
+                if(i % 13 != 0)
+                {
+                    soma += i;
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
---
+++ uri1133RestoDaDivisao2ou3/uri1133RestoDaDivisao2ou3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace uri1133RestoDaDivisao2ou3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            int maior, menor;
+            if (number1 > number2)
+            {
+                maior = number1;
+                menor = number2;
+            }
+            else
+            {
+                maior = number2;
+                menor = number1;
+            }
+
+
+            for (int i = menor + 1; i < maior; i++)
+            {
+
+                if (i % 5 == 2)
+                {
+                    Console.WriteLine(i);
+                }
+                else if (i % 5 == 3)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
