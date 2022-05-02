784bfed6e360d577aa664fc75033afdc3d67a52c
Michael Lima <michaellima.contato@gmail.com>
2022-05-01 22:06:07 -0300

    Atividades novas

---
+++ uri1071SomaDeImparesConsecutivos/uri1071SomaDeImparesConsecutivos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace uri1071SomaDeImparesConsecutivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, max, min, soma = 0;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            if (x > y)
+            {
+                max = x;
+                min = y;
+            }
+            else
+            {
+                min = x;
+                max = y;
+            }
+
+            for (int i = min; i < max; i++)
+            {
+                if (i % 2 != 0 && i!=min)
+                {
+                    soma += i;
+                }
+            }
+            Console.WriteLine(soma);
+
+        }
+    }
+}
