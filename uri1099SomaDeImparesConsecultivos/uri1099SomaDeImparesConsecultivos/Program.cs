8bbbe5076a634742c4383c44b1e5fef42e8b7e08
Michael Lima <michaellima.contato@gmail.com>
2022-05-03 21:07:04 -0300

    atividades novas

---
+++ uri1099SomaDeImparesConsecultivos/uri1099SomaDeImparesConsecultivos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace uri1099SomaDeImparesConsecultivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            int maior, menor;
+            for (int i = 0; i < numero; i++)
+            {
+                int soma = 0;
+                int[] vet = Console.ReadLine().Split(' ').Select(e => int.Parse(e)).ToArray();
+                if (vet[0] > vet[1])
+                {
+                    maior = vet[0];
+                    menor = vet[1];
+                }
+                else
+                {
+                    maior = vet[1];
+                    menor = vet[0];
+                }
+
+                for(int j =menor; j< maior; j++)
+                {
+                    if (j % 2 != 0 && j!=menor)
+                    {
+                        soma += j;
+                    }
+                }
+                Console.WriteLine(soma);
+                
+            }
+
+        }
+    }
+}
---
+++ uri1157Divisores/uri1157Divisores/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1157Divisores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
