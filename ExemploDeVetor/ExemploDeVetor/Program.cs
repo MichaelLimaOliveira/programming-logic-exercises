abfbdb52f7c50d1dfe3dcc5fa34bbd9b167e0571
Michael Lima <michaellima.contato@gmail.com>
2022-05-06 00:00:35 -0300

    exemplos de vetor

---
+++ ExemploDeVetor/ExemploDeVetor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExemploDeVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[] vet = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine());
+            }
+
+            for (int i=0; i < n; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+
+        }
+    }
+}
