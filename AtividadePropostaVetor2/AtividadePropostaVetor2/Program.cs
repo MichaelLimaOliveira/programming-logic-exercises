6198e2607dea39c8b701179798f726498bb544c8
Michael Lima <michaellima.contato@gmail.com>
2022-05-07 14:13:18 -0300

    atividades propostas vetores

---
+++ AtividadePropostaVetor2/AtividadePropostaVetor2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace AtividadePropostaVetor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numberInVet = Console.ReadLine()
+                                       .Split(' ')
+                                       .Select(n => int.Parse(n))
+                                       .OrderByDescending(n => n) //serve para ordenar do maior pro menor
+                                       .ToArray();
+
+            int cont = 0;
+
+            foreach (int number in numberInVet)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.Write(number + " ");
+                    cont++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(cont);
+        }
+    }
+}
