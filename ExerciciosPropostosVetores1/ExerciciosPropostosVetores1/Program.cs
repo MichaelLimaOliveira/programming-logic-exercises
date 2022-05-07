5e506ca6e2b8f00e0341cae7b7015d2c25f0a66f
Michael Lima <michaellima.contato@gmail.com>
2022-05-07 13:24:45 -0300

    atividades propostas vetor

---
+++ ExerciciosPropostosVetores1/ExerciciosPropostosVetores1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace ExerciciosPropostosVetores1
+{
+    class Program
+    {
+        public static NumberStyles Cult { get; private set; }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double higherNumber;
+            double higherNumberPosition = 0.0;
+            int maxPosition = 0;
+            double[] numberInVet = Console.ReadLine()
+                                          .Split(' ')
+                                          .Select(n => double.Parse(n,CultureInfo.InvariantCulture))
+                                          .ToArray();
+            
+            
+            higherNumber = numberInVet.Max();
+
+            for (int i = 0; i < n; i++)
+            {
+               
+                if (numberInVet[i] > higherNumberPosition)
+                {
+                    maxPosition = i;
+                    higherNumberPosition = numberInVet[i];
+                }
+            }
+            higherNumber = numberInVet.Max();
+
+            Console.WriteLine(higherNumber.ToString("F1"),CultureInfo.InvariantCulture);
+            Console.WriteLine(maxPosition);
+        }
+    }
+}
---
+++ ExerciciosPropostosVetores1/README.md
@@ -0,0 +1,6 @@
+Seção: Vetores
+Exercícios propostos
+
+
+1) Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida, mostrar na tela o maior número
+do vetor (supor não haver empates). Mostrar também a posição do maior elemento.@c
