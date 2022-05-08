5a820b7eee194d317cd7d323ef14d1dbb1da6085
Michael Lima <michaellima.contato@gmail.com>
2022-05-07 23:55:58 -0300

    atividades propostas vetor

---
+++ AtividadesPropostosVetor7/AtividadesPropostosVetor7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace AtividadesPropostosVetor7
+{
+    class Program
+    {
+        private static NumberStyles cul;
+
+        static void Main(string[] args)
+        {
+            int persons = int.Parse(Console.ReadLine());
+
+            string[] names = new string[persons];
+            double[] nota1 = new double[persons];
+            double[] nota2 = new double[persons];
+
+
+
+            for (int i = 0; i < persons; i++)
+            {
+                string[] aux = Console.ReadLine().Split(' ');
+                names[i] = aux[0];
+                nota1[i] = double.Parse(aux[1],CultureInfo.InvariantCulture);
+                nota2[i] = double.Parse(aux[2],CultureInfo.InvariantCulture);
+
+            }
+            Console.WriteLine("Alunos aprovados");
+            for (int i = 0; i < persons; i++)
+            {
+                double media = (nota1[i] + nota2[i]) / 2;
+
+
+                if (media >= 6)
+                {
+                    Console.WriteLine(names[i]);
+                }
+            }
+        }
+    }
+
+}
---
+++ AtividadesPropostosVetor7/README.md
@@ -0,0 +1,15 @@
+### Seção: Vetores
+## Exercícios propostos
+
+
+7) Fazer um programa para ler um conjunto de **N** nomes de alunos, bem como as notas que eles tiraram no 1º e 2º semestres. Cada uma dessas informações deve ser armazenada em um **vetor**. Depois, imprimir os nomes dos alunos aprovados, considerando aprovados aqueles cuja média das notas seja maior ou igual a seis.
+
+Exemplo:
+|Entrada|Saída|
+|---------------|------------------|
+|4                          | Alunos aprovados:
+|Joao &nbsp; 7.0 &nbsp;  8.5       |Joao
+|Maria &nbsp;9.2 &nbsp;6.5 |Maria
+|Carlos &nbsp;5.0 &nbsp;6.0 |Teresa
+|Teresa &nbsp;5.5 &nbsp;6.5
+
