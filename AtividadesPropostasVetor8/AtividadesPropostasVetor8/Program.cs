faa34be33e2ee0e399b97bfde1cae73facbbfc23
Michael Lima <michaellima.contato@gmail.com>
2022-05-08 15:08:45 -0300

    atividades propostas vetor

---
+++ AtividadesPropostasVetor8/AtividadesPropostasVetor8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+namespace AtividadesPropostasVetor8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] heights = new double[n];
+            char[] sex = new char[n];
+
+            double minHeight = 0.0;
+            double maxHeight = 0.0;
+
+            double soma = 0.0;
+            int contF = 0;
+            int contM = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] aux = Console.ReadLine().Split(' ');
+                heights[i] = double.Parse(aux[0], CultureInfo.InvariantCulture);
+                sex[i] = char.Parse(aux[1]);
+
+                if (sex[i] == 'F')
+                {
+                    soma += heights[i];
+                    contF++;
+                }
+
+                if (sex[i] == 'M')
+                {
+                    contM++;
+                }
+            }
+
+            minHeight = heights.Min();
+            maxHeight = heights.Max();
+
+            double media = soma / contF;
+
+
+
+            Console.WriteLine("Menor altura = " + minHeight.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Maior altura = " + maxHeight.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Media das alturas das mulheres = " + media.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Numero de homens = " + contM);
+
+
+        }
+    }
+}
---
+++ AtividadesPropostasVetor8/README.md
@@ -0,0 +1,18 @@
+### Seção: Vetores
+## Exercícios propostos
+
+
+8) Tem-se um conjunto de dados contendo a altura e o sexo (**M**, **F**) de **N** pessoas. Fazer um programa que calcule e escreva: 
+- a maior e a menor altura do grupo 
+- a média de altura das mulheres 
+- o número de homens
+
+Exemplo:
+|Entrada|Saída|
+|---------------|------------------|
+|5                         | Alunos aprovados:
+|1.70 F    |Menor altura = 1.54
+|1.83 M |Maior altura = 1.83
+|1.54 M |Media das alturas das mulheres = 1.69
+|1.61 F|Numero de homens = 2
+|1.75 F
