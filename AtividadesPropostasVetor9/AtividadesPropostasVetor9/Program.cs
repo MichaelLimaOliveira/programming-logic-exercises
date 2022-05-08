9244b43c11118d89701e5fb5f5bf5c3fdef0f824
Michael Lima <michaellima.contato@gmail.com>
2022-05-08 17:32:53 -0300

    atividades propostas de vetor

---
+++ AtividadesPropostasVetor9/AtividadesPropostasVetor9/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+namespace AtividadesPropostasVetor9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] produtos = new string[n];
+            double[] precoDeCompra = new double[n];
+            double[] precoDeVenda = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] aux = Console.ReadLine().Split(' ');
+                produtos[i] = aux[0];
+                precoDeCompra[i] = double.Parse(aux[1], CultureInfo.InvariantCulture);
+                precoDeVenda[i] = double.Parse(aux[2], CultureInfo.InvariantCulture);
+
+            }
+            double dezPorcento;
+            double vintePorcento;
+
+            int contAbaixoDeDez = 0;
+            int contAbaixoDeVinteAcimaDeDez = 0;
+            int contAcimaDeVinte = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                dezPorcento = (precoDeCompra[i] * 0.10) + precoDeCompra[i];
+                vintePorcento = (precoDeCompra[i] * 0.20) + precoDeCompra[i];
+
+                if (dezPorcento > precoDeVenda[i])
+                {
+                    contAbaixoDeDez++;
+                }
+                else if (precoDeVenda[i] >= dezPorcento && precoDeVenda[i] <= vintePorcento)
+                {
+                    contAbaixoDeVinteAcimaDeDez++;
+                }
+                else if (precoDeVenda[i] > vintePorcento)
+                {
+                    contAcimaDeVinte++;
+                }
+
+            }
+
+            double totalDeCompra = precoDeCompra.Sum();
+            double totalDeVenda = precoDeVenda.Sum();
+
+            double lucroTotal = totalDeVenda - totalDeCompra;
+
+
+            Console.WriteLine("Lucro abaixo de 10%: " + contAbaixoDeDez);
+            Console.WriteLine("Lucro entre 10% e 20%: " + contAbaixoDeVinteAcimaDeDez);
+            Console.WriteLine("Lucro acima de 20%: " + contAcimaDeVinte);
+            Console.WriteLine("Valor total de compra: " + totalDeCompra.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor total de venda: " + totalDeVenda.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Lucro total: " + lucroTotal.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ AtividadesPropostasVetor9/README.md
@@ -0,0 +1,18 @@
+### Seção: Vetores
+## Exercícios propostos
+
+
+9) Um comerciante deseja fazer o levantamento do lucro das mercadorias que ele comercializa. Para isto, mandou digitar um conjunto de N mercadorias, cada uma contendo nome, preço de compra e preço de venda das mesmas. Fazer um programa que leia tais dados e determine e escreva quantas mercadorias proporcionaram:
+-  lucro< 10%
+- 10% ≤ lucro ≤ 20%
+- lucro> 20%
+
+Exemplo:
+Entrada|Saída|
+---------------|------------------|
+4                        |Lucro abaixo de 10%: 1
+Feijao &nbsp;10.00 &nbsp;11.00  |Lucro entre 10% e 20%: 2
+Arroz &nbsp;12.00 &nbsp;12.80   |Lucro acima de 20%: 1
+Oleo &nbsp;5.00 &nbsp;5.70      |Valor total de compra: 30.00
+Sal &nbsp;3.00 &nbsp;4.00       |Valor total de venda: 33.50
+|                               |Lucro total: 3.50                     
