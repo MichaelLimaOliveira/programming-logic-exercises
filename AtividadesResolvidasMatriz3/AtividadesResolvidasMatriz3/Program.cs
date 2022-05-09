88ec6344e5e5ecf0c740b4e050300091b7bf6358
Michael Lima <michaellima.contato@gmail.com>
2022-05-09 20:33:35 -0300

    atividades resolvidas matriz

--- AtividadesResolvidasMatriz3/AtividadesResolvidasMatriz3/Program.cs
+++ AtividadesResolvidasMatriz3/AtividadesResolvidasMatriz3/Program.cs
@@ -6,7 +6,27 @@ namespace AtividadesResolvidasMatriz3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int L, C;
+            string[] s = Console.ReadLine().Split(' ');
+            L = int.Parse(s[0]);
+            C = int.Parse(s[1]);
+
+            int[,] matriz = new int[L, C];
+
+            int soma;
+
+            for (int i = 0; i < L; i++)
+            {
+                soma = 0;
+                s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < C; ++j)
+                {
+                    matriz[i, j] = int.Parse(s[j]);
+
+                    soma += matriz[i,j];
+                }
+                Console.WriteLine(soma);
+            }
         }
     }
 }
