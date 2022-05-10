c4b9b4619dce90a4a915baa22b3ae0adbcddc73a
Michael Lima <michaellmo@outlook.com>
2022-05-10 14:27:53 -0300

    Atividades Propostas Matriz

---
+++ AtividadesPropostasMatriz6/AtividadesPropostasMatriz6/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+namespace AtividadesPropostasMatriz6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[,] matizSquare = new double[n, n];
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] aux = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; ++j)
+                {
+                    matizSquare[i, j] = double.Parse(aux[j], CultureInfo.InvariantCulture);
+                }
+
+            }
+
+            int readLine = int.Parse(Console.ReadLine());
+            int readColumn = int.Parse(Console.ReadLine());
+
+            double somaPositive = 0.0;
+
+            foreach (double e in matizSquare)
+            {
+                if (e > 0)
+                {
+                    somaPositive += e;
+                }
+            }
+
+
+            Console.WriteLine("SOMA DOS POSITIVOS: " + somaPositive.ToString("F1", CultureInfo.InvariantCulture));
+            Console.Write("LINHA ESCOLHIDA: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(matizSquare[readLine, i].ToString("F1", CultureInfo.InvariantCulture) + " ");
+
+
+            }
+            Console.WriteLine();
+
+            Console.Write("COLUNA ESCOLHIDA: ");
+            for (int i = 0; i < n; ++i)
+            {
+                Console.Write(matizSquare[i, readColumn].ToString("F1", CultureInfo.InvariantCulture) + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("DIAGONAL PRINCIPAL: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(matizSquare[i, i].ToString("F1", CultureInfo.InvariantCulture) + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("MATRIZ ALTERADA: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (matizSquare[i, j] < 0)
+                    {
+                        matizSquare[i, j] = Math.Pow(matizSquare[i, j], 2);
+                        Console.Write(matizSquare[i, j].ToString("F1", CultureInfo.InvariantCulture) + " ");
+
+                    }
+                    else
+                    {
+                        Console.Write(matizSquare[i, j].ToString("F1", CultureInfo.InvariantCulture) + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+
+    }
+}
---
+++ AtividadesPropostasMatriz6/README.md
@@ -0,0 +1,30 @@
+
+### Seção: Matrizes
+## Exercícios propostos
+
+
+
+6) Ler uma matriz quadrada de ordem **N**, contendo números reais. Em seguida, fazer as seguintes ações:
+a) calcular e imprimir a soma de todos os elementos positivos da matriz. 
+b) fazer a leitura do índice de uma linha da matriz e, daí, imprimir todos os elementos desta linha. 
+c) fazer a leitura do índice de uma coluna da matriz e, daí, imprimir todos os elementos desta coluna. 
+d) imprimir os elementos da diagonal principal da matriz. 
+e) alterar a matriz elevando ao quadrado todos os números negativos da mesma. Em seguida imprimir a matriz alterada.
+
+### Entrada: 
+A entrada contém o número inteiro **N**, seguido dos valores da matriz com uma casa decimal cada, seguido do índice de uma linha, seguido do índice de uma coluna, conforma exemplo.
+
+### Saída:
+A saída contém os valores de saída de cada ação, com uma casa decimal, na ordem em que foram apresentadas no enunciado, conforme exemplo.
+
+Exemplo:
+|Entrada|Saída|
+|---------------|------------------|
+3 |SOMA DOS POSITIVOS: 40.0
+7.0 -8.0 10.0 |LINHA ESCOLHIDA: -2.0 3.0 5.0
+-2.0 3.0 5.0 |COLUNA ESCOLHIDA: 10.0 5.0 4.0
+11.0 -15.0 4.0 |DIAGONAL PRINCIPAL: 7.0 3.0 4.0
+1 |MATRIZ ALTERADA:
+2|7.0 64.0 10.0
+&nbsp; |4.0 3.0 5.0
+&nbsp; |11.0 225.0 4.0
