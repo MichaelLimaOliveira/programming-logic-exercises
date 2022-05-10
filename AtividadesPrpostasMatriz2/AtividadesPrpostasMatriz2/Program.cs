42914dd8bb3b09063108967c35c38a415ab9b08f
Michael Lima <michaellmo@outlook.com>
2022-05-10 09:52:51 -0300

    atividades prppostas matriz

---
+++ AtividadesPrpostasMatriz2/AtividadesPrpostasMatriz2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AtividadesPrpostasMatriz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, n];
+
+            int soma;
+            for (int i = 0; i < n; ++i)
+            {
+                string[] aux = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; ++j)
+                {
+                    matriz[i, j] = int.Parse(aux[j]);
+                }
+            }
+
+
+            for (int i = 0; i < n; ++i)
+            {
+                soma = 0;
+                for (int j = 0; j < n; ++j)
+                {
+                    soma += matriz[i, j];
+                }
+                Console.WriteLine(soma);
+            }
+        }
+
+    }
+}
---
+++ AtividadesPrpostasMatriz2/README.md
@@ -0,0 +1,21 @@
+
+### Seção: Matrizes
+## Exercícios propostos
+
+
+
+2) Ler um número **N** e depois uma matriz quadrada **NxN** com números inteiros. Depois, mostrar na tela a soma dos elementos de cada linha da matriz.
+
+### Entrada: 
+A entrada contém o número **N**, depois os dados da matriz.
+
+### Saída:
+ A saída contém os números representando a soma dos elementos de cada linha da matriz.
+
+Exemplo:
+|Entrada|Saída|
+|---------------|------------------|
+ 3      |11
+ 5 &nbsp;2 &nbsp;4|19
+ 10 &nbsp;3 &nbsp;6|29
+  9 &nbsp;8 &nbsp;12|
