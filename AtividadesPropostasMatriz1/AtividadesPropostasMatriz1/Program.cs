1615c3d9c871ebdb4d64e069aa6ab8caf2dae79b
Michael Lima <michaellmo@outlook.com>
2022-05-09 21:35:42 -0300

    Atividades

---
+++ AtividadesPropostasMatriz1/AtividadesPropostasMatriz1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AtividadesPropostasMatriz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int L, C;
+            string[] aux = Console.ReadLine().Split(' ');
+            L = int.Parse(aux[0]);
+            C = int.Parse(aux[1]);
+
+            int[,] M = new int[L, C];
+
+            for (int i = 0; i < L; ++i)
+            {
+                aux = Console.ReadLine().Split(' ');
+                for (int j = 0; j<C; ++j)
+                {
+                    M[i, j] = int.Parse(aux[j]);
+                }
+            }
+
+            Console.WriteLine("VALORES NEGATIVOS:");
+
+            foreach(int m in M)
+            {
+                if(m < 0)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+        }
+    }
+}
---
+++ ExercicioVogaisExtraterrestres/ExercicioVogaisExtraterrestres/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ExercicioVogaisExtraterrestres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frasseDeTeste = Console.ReadLine();
+            string frase = Console.ReadLine();
+            int cont = frase.Where(c => frasseDeTeste.Contains(c)).Count();
+            Console.WriteLine(cont);
+        }
+    }
+}
